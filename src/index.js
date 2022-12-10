26760508895c47aa9f121681b21efc7f4501824f
GeralmyUrena <geralmyuri11@gmail.com>
2022-12-09 22:06:28 -0500

    commit tree cb3bc37290f47dddeef843d750a2c10e16ac89e9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+    
+        console.log(input.value);
+    
+    fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');//document.getElementById("t")//seems you need to get section elements to target childs
+            const summary = document.querySelector('section#movieDetails p');//document.getElementById("s")
+            console.log(title)
+            console.log(summary)
+            title.innerText = data.title;
+            summary.innerText = data.summary;   
+          
+          // LOG: (3) [{…}, {…}, {…}]
+        });
+      });
+    }
+    
 
 document.addEventListener('DOMContentLoaded', init);
